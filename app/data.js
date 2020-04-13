2988d0a0d10cb6732227d60bda2803dd29e12bb2
tremendo <47454976+tremendoagency@users.noreply.github.com>
2020-04-13 01:01:30 -0300

    Add YT hotspot

--- app/data.js
+++ app/data.js
@@ -31,4 +31,19 @@ var APP_DATA = {
     "fullscreenButton": true,
     "viewControlButtons": true
   }
+  // Get the hotspot container for scene.
+var container = scene.hotspotContainer();
+"infoHotspots": [
+  {
+    "yaw": -0.1457067243766943,
+    "pitch": -0.006392650344780648,
+    "title": "Title",
+    "text": "Text"
+  }
+]
+// Create hotspot with different sources.
+container.createHotspot(document.getElementById('iframespot'), { yaw: -0.1457067243766943, pitch: -0.006392650344780648 },
+  { perspective: { radius: 1640, extraTransforms: "rotateX(5deg)" }});
+wrapper.innerHTML = '<iframe id="youtube" width="1280" height="480" src="https://www.youtube.com/embed/a4YjKmsXyds?rel=0&amp;controls=0&amp;showinfo=0&amp;" frameborder="0" allowfullscreen></iframe>';
+
 };
