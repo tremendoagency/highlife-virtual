516b02df2c36c2dcc1dfeb4b72cddecaeeee6d19
tremendo <47454976+tremendoagency@users.noreply.github.com>
2020-04-15 16:10:10 -0300

    change test app

--- test/app-files/data.js
+++ test/app-files/data.js
@@ -1,70 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-bg",
-      "name": "BG",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1500,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-bg2",
-      "name": "bg2",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1500,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-bg3",
-      "name": "bg3",
+      "id": "0-bg13",
+      "name": "bg13",
       "levels": [
         {
           "tileSize": 256,
