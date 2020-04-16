679074127dffef4aabd6f951e5df7ae58b16bd5f
tremendo <47454976+tremendoagency@users.noreply.github.com>
2020-04-16 17:20:55 -0300

    move test app

---
+++ test/app-files/data.js
@@ -0,0 +1,67 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-main",
+      "name": "main",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.029403488673125366,
+          "pitch": -0.055776435405626046,
+          "title": "Main Stage",
+          "text": "Text"
+        },
+        {
+          "yaw": -3.1252195779530645,
+          "pitch": -0.2324644897962891,
+          "title": "Cocktail Stage",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.581181933515758,
+          "pitch": -0.2782678862177157,
+          "title": "Food &amp; Drinks",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.6537812028496468,
+          "pitch": -0.17792343649880848,
+          "title": "Photobooth",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
